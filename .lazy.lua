6427d02d222bd7eda8f7ff6350e1de0110121a0c
Filip Godlewski <54432731+filipgodlewski@users.noreply.github.com>
2025-06-25 20:46:40 +0200

    simplify my .lazy.nvim config

--- .lazy.lua
+++ .lazy.lua
@@ -2,9 +2,12 @@ return {
   {
     "stevearc/conform.nvim",
     optional = true,
-    opts = function(_, opts)
-      table.insert(opts.formatters.pyupgrade.args, 1, "--py312-plus")
-      opts.formatters_by_ft.python = { "pyupgrade", "ruff_fix", "ruff_format" }
-    end,
+    opts = {
+      formatters = {
+        pyupgrade = {
+          prepend_args = "--py312-plus",
+        },
+      },
+    },
   },
 }

16fa9a48a40f62c3d6e841cf35a3e244df1cc968
Filip Godlewski <54432731+filipgodlewski@users.noreply.github.com>
2025-06-26 14:59:56 +0200

    simplify pytest arg for e2e tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,17 +12,17 @@ e2e = pytest.mark.e2e
 
 
 def pytest_addoption(parser: "Parser"):
-    parser.addoption("--run-e2e", action="store_true", default=False, help="run end-to-end tests")
+    parser.addoption("--e2e", action="store_true", default=False, help="run end-to-end tests")
 
 
 def pytest_collection_modifyitems(config: "Config", items: list["Item"]) -> None:
-    if config.getoption("--run-e2e"):
+    if config.getoption("--e2e"):
         required_env_variables = ["E2E_SMTP_EMAIL", "E2E_SMTP_X_API_KEY", "E2E_TARGET_DOMAIN"]
         if missing := [v for v in required_env_variables if not os.getenv(v)]:
-            pytest.exit(f"Missing required environment variables for --run-e2e: {", ".join(missing)}")
+            pytest.exit(f"Missing required environment variables for --e2e: {", ".join(missing)}")
         return
 
-    skip_e2e = pytest.mark.skip(reason="use --run-e2e to run end-to-end tests")
+    skip_e2e = pytest.mark.skip(reason="use --e2e to run end-to-end tests")
     for item in items:
         if "e2e" in item.keywords:
             item.add_marker(skip_e2e)

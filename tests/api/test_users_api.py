befe365208ed43270f7c7612688c58638737db57
Filip Godlewski <54432731+filipgodlewski@users.noreply.github.com>
2025-07-29 18:51:43 +0200

    add tests for users api (+e2e)

--- tests/api/test_users_api.py
+++ tests/api/test_users_api.py
@@ -1,3 +1,4 @@
+import datetime
 import re
 from unittest import mock
 from uuid import uuid1
@@ -5,15 +6,18 @@ from uuid import uuid1
 import pytest
 from pytest_mock import MockerFixture
 
+from boox.api.core import TokenMissingError
 from boox.api.users import UsersApi
 from boox.core import Boox
 from boox.models.enums import BooxUrl
 from boox.models.users import (
+    DataSession,
     DataToken,
     FetchTokenRequest,
     FetchTokenResponse,
     SendVerifyCodeRequest,
     SendVerifyResponse,
+    SyncTokenResponse,
 )
 from tests.api.utils import E2EConfig, EmailProvider
 from tests.conftest import e2e
@@ -62,21 +66,19 @@ def test_users_api_send_verification_code_integration(mocker: MockerFixture, moc
 
 
 def test_fetch_session_token_calls_post_and_parses_response(mocker: MockerFixture):
-    token = uuid1()
+    token = str(uuid1())
     mocked_session = mocker.Mock()
     api = UsersApi(session=mocked_session)
 
     mocked_response = mocker.Mock()
-    mocked_response.json = mocker.Mock(
-        return_value={"data": {"token": str(token)}, "message": "SUCCESS", "result_code": 0}
-    )
+    mocked_response.json = mocker.Mock(return_value={"data": {"token": token}, "message": "SUCCESS", "result_code": 0})
 
     api._post = mocker.Mock(return_value=mocked_response)
 
     payload = FetchTokenRequest(mobi="foo@bar.com", code="123456")
     result = api.fetch_session_token(payload=payload)
 
-    data = FetchTokenResponse.model_validate({"data": {"token": str(token)}, "message": "SUCCESS", "result_code": 0})
+    data = FetchTokenResponse.model_validate({"data": {"token": token}, "message": "SUCCESS", "result_code": 0})
     assert result == data
     api._post.assert_called_once_with(
         endpoint="/api/1/users/signupByPhoneOrEmail", json={"mobi": "foo@bar.com", "code": "123456"}
@@ -85,9 +87,9 @@ def test_fetch_session_token_calls_post_and_parses_response(mocker: MockerFixtur
 
 @pytest.mark.parametrize("url", list(BooxUrl))
 def test_users_api_fetch_session_token_integration(mocker: MockerFixture, mocked_client: mock.Mock, url: BooxUrl):
-    token = uuid1()
+    token = str(uuid1())
     mocked_response = mocker.Mock()
-    mocked_response.json.return_value = {"data": {"token": str(token)}, "message": "SUCCESS", "result_code": 0}
+    mocked_response.json.return_value = {"data": {"token": token}, "message": "SUCCESS", "result_code": 0}
     mocked_response.raise_for_status.return_value = mocked_response
     mocked_client.post.return_value = mocked_response
 
@@ -100,8 +102,43 @@ def test_users_api_fetch_session_token_integration(mocker: MockerFixture, mocked
     mocked_client.post.assert_called_once_with(expected_url, json=expected_json)
     mocked_response.json.assert_called_once()
     assert isinstance(result, FetchTokenResponse)
-    data = DataToken.model_validate({"token": str(token)})
+    data = DataToken.model_validate({"token": token})
     assert result.data == data
+
+
+@pytest.mark.parametrize("url", list(BooxUrl))
+def test_1(mocker: MockerFixture, mocked_client: mock.Mock, url: BooxUrl):
+    token = str(uuid1())
+    mocked_response = mocker.Mock()
+    data = {
+        "channels": (),
+        "cookie_name": "foo",
+        "expires": datetime.datetime.now(tz=datetime.UTC),
+        "session_id": "xyz123",
+    }
+    mocked_response.json = mocker.Mock(
+        return_value={"data": data, "message": "SUCCESS", "result_code": 0, "tokenExpiredAt": 1}
+    )
+    mocked_response.raise_for_status.return_value = mocked_response
+    mocked_client.get.return_value = mocked_response
+
+    with Boox(client=mocked_client, base_url=url, token=token) as boox:
+        result = boox.users.synchronize_token()
+
+    expected_url = url.value + "/api/1/users/syncToken"
+    mocked_client.get.assert_called_once_with(expected_url)
+    mocked_response.json.assert_called_once()
+    assert isinstance(result, SyncTokenResponse)
+    assert result.data == DataSession.model_validate(data)
+
+
+@pytest.mark.parametrize("url", list(BooxUrl))
+def test_2(mocked_client: mock.Mock, url: BooxUrl):
+    with (
+        Boox(client=mocked_client, base_url=url) as boox,
+        pytest.raises(TokenMissingError, match="Bearer token is required to call this method"),
+    ):
+        boox.users.synchronize_token()
 
 
 @e2e
@@ -120,6 +157,7 @@ def test_send_verification_code_e2e(config: E2EConfig, email: EmailProvider):
 
 
 @e2e
+@pytest.mark.order(1)
 def test_fetch_session_token_e2e(config: E2EConfig):
     if not config.verification_code:
         pytest.skip("Verification code was either not obtained or not set")
